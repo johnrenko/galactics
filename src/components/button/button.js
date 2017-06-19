a7808c1b2dbe94ad4ce69924fb4475e21878cdf8
arnaudlrx <lrxarnaud@gmail.com>
2017-06-19 17:19:09 +0200

    📦 SASS Packages added

---
+++ src/components/button/button.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as styles from './button.css';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="button">Text button</button>
+    );
+  }
+};
+
+export default Button;
